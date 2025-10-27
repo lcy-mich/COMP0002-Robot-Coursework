693b3b66f5618243c0577c5eca4b63136588f3eb
lcy-mich <michaelcyluk@outlook.com>
2025-10-27 15:38:54 +0000

    refactor and organisation

--- grid.c
+++
@@ -1,6 +0,0 @@
-#include "grid.h"
-#include "vectors.h"
-
-int getIndexFromVector(Vector pos, int grid_size_y) {
-    return pos.x + pos.y*grid_size_y;
-}
--- main.c
+++
@@ -1,3 +0,0 @@
-#include "drawapp.4.5/graphics.h"
-#include "vectors.h"
-#include "robot.h"
